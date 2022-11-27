79b8cc7db5b0187e86f8e169f7ab4f4270b5a4b8
strahpas <strahpas1986@yandex.ru>
2022-11-27 17:17:34 +0300

    добавить кнопку удаления

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const close = document.querySelectorAll('.popup__close'),
       popupAdd = document.querySelector('.popup_add'),
       saveClose = document.querySelector('.popup__button');
 
+
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__input_form_name');
 let jobInput = document.querySelector('.popup__input_form_job');
@@ -39,6 +40,53 @@ function formSubmitHandler (evt) {
 
     closePopUp();
 }
+
+// добавление карточек
+
+const initialCards = [
+    {
+      name: 'Карачаевск',
+      link: './images/Karachaevsk.jpg'
+    },
+    {
+      name: 'Гора Эльбрус',
+      link: './images/Elbrus.jpg'
+    },
+    {
+      name: 'Домбай',
+      link: './images/Dombay.jpg'
+    },
+    {
+      name: 'Карелия',
+      link: './images/Karelia.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: './images/Kamchatka.jpg'
+    },
+    {
+      name: 'Карачаево-Черкессия',
+      link: './images/Karachaevo.jpg'
+    }
+  ];
+
+// const initialListElement = document.querySelector('.element');
+// const initialTemplate = document.querySelector('#element-template').content.querySelector('.element');
+
+// function createElement(item) {
+//   const elementAdd = initialTemplate.cloneNode(true);
+//   const elementTitle = elementAdd.querySelector('.element');
+//   elementTitle.textContent = item.title;
+
+//   initialListElement.append(elementAdd);
+// }
+
+// initialListElement.forEach(function (item) {
+//   createElement(item);
+// });
+
+// функционал лайков
+
 
 open.addEventListener('click', openPopUp);
 openAddBtn.addEventListener('click', openPopupAdd);
